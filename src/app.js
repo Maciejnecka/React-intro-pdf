24ad0bc915767e8fb3a33c84b7f76027c3ba8ce8
Maciej Necka <macieknecka@gmail.com>
2023-11-04 12:01:01 +0100

    35/84 #01 Wyrazenia

--- src/app.js
+++ src/app.js
@@ -4,4 +4,22 @@ import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.querySelector('#root'));
 
-root.render(<h1>Hello World</h1>);
+// const myStyles = {
+//   border: '1px solid blue',
+//   backgroundColor: 'yellow',
+// };
+
+// root.render(
+//   <h1 className="myClass" style={myStyles}>
+//     Hello World
+//   </h1>
+// );
+
+const output = (
+  <section>
+    <img src="https://www.google.pl/favicon.ico" />
+    <input />
+  </section>
+);
+
+root.render(output);

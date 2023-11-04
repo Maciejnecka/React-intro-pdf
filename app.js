006e2a0c4904b60bbeeb8e1f30b8a766c59f716a
Maciej Necka <macieknecka@gmail.com>
2023-11-04 11:40:46 +0100

    18/84 #02 Webpack

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 'use strict';
-console.log('Test');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const element = React.createElement('h1', null, 'Hello world');
+
+root.render(element);
+
+// ReactDOM.render(
+//   React.createElement('h1', null, 'Hello World'),
+//   document.querySelector('#root')
+// );
+
+// Uwaga: jeśli chcesz przetestować to rozwiązanie
+// w pliku index.html zmien wersje
+// react@17 i react-dom@17 na 18

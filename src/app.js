c658d07a1ca79d44a2c0e62e890c9f2e96c1bdcb
Maciej Necka <macieknecka@gmail.com>
2023-11-04 11:51:08 +0100

    27/84 #3 JSX

---
+++ src/app.js
@@ -0,0 +1,7 @@
+'use strict';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.querySelector('#root'));
+
+root.render(<h1>Hello World</h1>);
